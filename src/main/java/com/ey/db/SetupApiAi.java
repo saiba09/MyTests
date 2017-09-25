de51b10963646562e133d5c5a079068d9256fead
saiba09 <saiba_sheikh@persistent.co.in>
2017-09-25 14:32:24 +0530

    added follup names

--- src/main/java/com/ey/db/SetupApiAi.java
+++ src/main/java/com/ey/db/SetupApiAi.java
@@ -1,9 +1,14 @@
 package com.ey.db;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.logging.Logger;
 
 import org.apache.http.HttpResponse;
@@ -11,6 +16,12 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -26,7 +37,8 @@ import org.json.simple.JSONArray;
 public class SetupApiAi {
 	static String USER_AGENT = "Mozilla/5.0";
 	private static final Logger log = Logger.getLogger(SetupApiAi.class.getName());
-	static String url = "https://api.api.ai/v1/intents?v=20150910";
+	static String baseUrl = "https://api.api.ai/v1/";
+	static String urlVersion = 	"?v=20150910";
 	static String contentType = "application/json";
 	static String auth = "Bearer 4d8addaa7dce4bf9a07af12182ab6922";
 
@@ -141,7 +153,7 @@ public class SetupApiAi {
 	public static String addIntent(String intent, String parentId,String rootParentId ) {
 		log.info("add intent()");
 		HttpClient client = HttpClientBuilder.create().build();
-		HttpPost post = new HttpPost(url);
+		HttpPost post = new HttpPost(baseUrl + "intents"+urlVersion);
 		StringBuffer responseBuffer = new StringBuffer();
 		// add header
 		post.setHeader("User-Agent", USER_AGENT);
@@ -174,7 +186,7 @@ public class SetupApiAi {
 				}
 				log.info("parsed responsed");
 			} catch (IOException e) {
-				log.info("Exception : " + e);
+				log.severe("Exception : " + e);
 			}
 		}
 
@@ -191,8 +203,8 @@ public class SetupApiAi {
 		return entityObject;
 	}
 	public static String addEntity(String entity){
-		log.info("add intent()");
-		String addEntityUrl = "https://api.api.ai/v1/entities?v=20150910";
+		log.info("addEntity()");
+		String addEntityUrl = baseUrl+"entities"+urlVersion;
 		HttpClient client = HttpClientBuilder.create().build();
 		HttpPost post = new HttpPost(addEntityUrl);
 		StringBuffer responseBuffer = new StringBuffer();
@@ -220,8 +232,10 @@ public class SetupApiAi {
 					responseBuffer.append(line);
 				}
 				log.info("parsed responsed");
+				String entityId = getId(responseBuffer.toString());
+				addElements( entityId , entity);
 			} catch (IOException e) {
-				log.info("Exception : " + e);
+				log.severe("Exception : " + e);
 			}
 			
 			
@@ -229,4 +243,116 @@ public class SetupApiAi {
 		return responseBuffer.toString();
 
 	}
+
+	private static String addElements(String entityId, String entity) {
+
+		log.info("add addElements()");
+		String addEntityUrl = baseUrl+"entities/"+entityId+"/entries"+urlVersion;
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpPost post = new HttpPost(addEntityUrl);
+		StringBuffer responseBuffer = new StringBuffer();
+		// add header
+		post.setHeader("User-Agent", USER_AGENT);
+		post.setHeader("Content-Type", contentType);
+		post.setHeader("Authorization",auth);
+		StringEntity stringEntity;
+		log.info("getting json file for elements of : "+ entity);
+		JSONObject entityObject = getEntityElementsJSON(entity);
+		log.info("json : "+entityObject.toJSONString());
+		if (entityObject != null) {
+			log.info("json fetched");
+			
+			try {
+				stringEntity = new StringEntity(entityObject.toJSONString());
+				log.info("posting to API AI");
+				post.setEntity(stringEntity);
+				HttpResponse response = client.execute(post);
+				log.info("got response : "+ response);
+				BufferedReader rd = new BufferedReader(new InputStreamReader(
+						response.getEntity().getContent()));
+				String line = "";
+				while ((line = rd.readLine()) != null) {
+					responseBuffer.append(line);
+				}
+				log.info("parsed responsed");
+				
+			} catch (IOException e) {
+				log.severe("Exception : " + e);
+			}
+			
+			
+		}
+
+	return responseBuffer.toString();
+		
+	}
+
+	private static JSONObject getEntityElementsJSON(String entity) {
+		
+		JSONObject elementJson = new JSONObject();
+		LinkedHashMap<String,String[]> list = new LinkedHashMap<>();
+		try {
+			String path = SetupApiAi.class.getResource("/us_states.xlsx")
+					.getPath();
+			//System.out.println(path);
+			FileInputStream excelFile = new FileInputStream(new File(path));
+			Workbook workbook = new XSSFWorkbook(excelFile);
+			Sheet datatypeSheet = workbook.getSheetAt(0);
+			Iterator<Row> iterator = datatypeSheet.iterator();
+			String[] cRow = new String[10];
+			int index = 0;
+			while (iterator.hasNext()) {
+				index = 0;
+				Row currentRow = iterator.next();
+				Iterator<Cell> cellIterator = currentRow.iterator();
+
+				while (cellIterator.hasNext()) {
+					Cell currentCell = cellIterator.next();
+					// getCellTypeEnum shown as deprecated for version 3.15
+					// getCellTypeEnum ill be renamed to getCellType starting
+					// from version 4.0
+
+					if (currentCell.getCellTypeEnum() == CellType.STRING) {
+						cRow[index] = currentCell.getStringCellValue();
+						index++;
+
+
+
+					} else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
+
+
+						cRow[index] = currentCell.getStringCellValue();
+
+					}
+				}
+				String [] synonyms = new String[]{cRow[1],cRow[2]};
+				list.put(cRow[0],synonyms);
+			}
+			workbook.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		JSONArray elementJsonArray = new JSONArray();
+		for (String element : list.keySet()) {
+			JSONArray entries = new JSONArray();
+			JSONObject entriesJson = new JSONObject();
+			JSONArray synonymsJson = new JSONArray();
+			
+			String[] synonyms = list.get(entries);
+			synonymsJson.add(synonyms[0]);
+			synonymsJson.add(synonyms[1]);
+			
+			entriesJson.put("value", element);
+			entriesJson.put("synonyms", synonymsJson);
+			entries.add(entriesJson);
+			elementJson.put("name", element);
+			elementJson.put("entries", entries);
+
+			
+		}
+
+		return elementJson;
+	
+	}
+	
 }
